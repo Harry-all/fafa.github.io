b5cfb725726ca54d8789284160fa581a109d48a7
Harry Galindo <harrygalindo29@gmail.com>
2024-07-30 21:33:43 -0500

    Lets See

---
+++ Home/Js/index.js
@@ -0,0 +1,7 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.Scroll',{
+    duration: 3000,
+    origin: 'bottom',
+    distance: '100px'
+});
---
+++ me/Js/index.js
@@ -0,0 +1,7 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.Scroll1',{
+    duration: 3000,
+    origin: 'bottom',
+    distance: '100px'
+});
